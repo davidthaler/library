510a8a24739ab0ba8b0d663a1cc2bc8fe349a615
David Thaler <davidthaler@gmail.com>
2020-07-28 23:25:38 -0700

    Committing version that uses CDN for Bootstrap.

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 let express = require('express')
+let chalk = require('chalk')
+let debug = require('debug')('app')
+let morgan = require('morgan')
+let path = require('path')
 
 let app = express()
+app.use(morgan('tiny'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res){
-    res.send('Hello from the library app!')
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
 app.listen(3000, function(){
-    console.log('listening on port 3000')
+    debug(`listening on port ${chalk.green('3000')}`)
 })
