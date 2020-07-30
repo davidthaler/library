e5788d32d66057a5f0cf8eea2e805afca45b8314
David Thaler <davidthaler@gmail.com>
2020-07-29 23:41:16 -0700

    set up eslint

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-let express = require('express')
-let chalk = require('chalk')
-let debug = require('debug')('app')
-let morgan = require('morgan')
-let path = require('path')
+const express = require('express')
+const chalk = require('chalk')
+const debug = require('debug')('app')
+const morgan = require('morgan')
+const path = require('path')
 
-let app = express()
+const app = express()
 app.use(morgan('tiny'))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/css', express.static(
-    path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css')))
+    path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css')
+))
 app.use('/js', express.static(
-    path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'js')))
+    path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'js')
+))
+
 app.use('/js', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')))
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
-app.listen(3000, function(){
+app.listen(3000, () => {
     debug(`listening on port ${chalk.green('3000')}`)
 })
