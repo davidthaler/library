ac6a743bb185a121cac308f159b055cbc60387f0
David Thaler <davidthaler@gmail.com>
2020-08-12 22:08:54 -0700

    chapter 9 refactor completed

---
+++ src/controllers/bookController.js
@@ -0,0 +1,60 @@
+const {MongoClient, ObjectID} = require('mongodb')
+const debug = require('debug')('app:bookController')
+const url = 'mongodb://localhost:27017'
+const dbName = 'libraryApp'
+
+function bookController(nav){
+
+    function getIndex(req, res){
+        // This semicolon is *required*.
+        // Automatic semicolon insertion fails here.
+        (async function gomongo(){
+            let client
+            try {
+                client = await MongoClient.connect(url)
+                debug('Connected in book controller...')
+                const db = client.db(dbName)
+                const coll = db.collection('books')
+                const books = await coll.find().toArray()
+                debug(books)
+                res.render('bookListView', {nav, books})
+            } catch (err) {
+                debug(err.stack)
+            }
+            client.close()
+        }())
+    }
+
+    async function getById(req, res) {
+        const { id } = req.params
+        let client
+        try {
+            client = await MongoClient.connect(url)
+            debug('Connected in book controller...')
+            const db = client.db(dbName)
+            const coll = db.collection('books')
+            const book = await coll.findOne({_id: ObjectID(id)})
+            debug(book)
+            res.render('bookView', {nav, book})
+        }catch (err){
+            debug(err)
+        }
+        client.close()
+    }
+
+    function middleware(req, res, next){
+        if(req.user){
+            next()
+        }else{
+            res.redirect('/')
+        }
+    }
+
+    return {
+        getIndex,
+        getById,
+        middleware
+    }
+}
+
+module.exports = bookController
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,58 +1,13 @@
 const express = require('express')
-const {MongoClient, ObjectID} = require('mongodb')
-const debug = require('debug')('app:bookRoutes')
+const bookController = require('../controllers/bookController')
+
 const bookRouter = express.Router()
 
 function router(nav){
-    bookRouter.use((req, res, next) => {
-        if(req.user){
-            next()
-        }else{
-            res.redirect('/')
-        }
-    })
-    bookRouter.route('/')
-        .get((req, res) => {
-            const url = 'mongodb://localhost:27017'
-            // This semicolon is *required*.
-            // Automatic semicolon insertion fails here.
-            const dbName = 'libraryApp';
-            (async function gomongo(){
-                let client
-                try {
-                    client = await MongoClient.connect(url)
-                    debug('Connected in book list...')
-                    const db = client.db(dbName)
-                    const coll = await db.collection('books')
-                    const books = await coll.find().toArray()
-                    debug(books)
-                    res.render('bookListView', {nav, books})
-                } catch (err) {
-                    debug(err.stack)
-                }
-                client.close()
-            }())
-        })
-    
-    bookRouter.route('/:id')
-        .get(async (req, res) => {
-            const { id } = req.params
-            const url = 'mongodb://localhost:27017'
-            const dbName = 'libraryApp'
-            let client
-            try {
-                client = await MongoClient.connect(url)
-                debug('Connected in book view...')
-                const db = client.db(dbName)
-                const coll = await db.collection('books')
-                const book = await coll.findOne({_id: ObjectID(id)})
-                debug(book)
-                res.render('bookView', {nav, book})
-            }catch (err){
-                debug(err)
-            }
-            client.close()
-        })
+    const {getIndex, getById, middleware} = bookController(nav)
+    bookRouter.use(middleware)
+    bookRouter.route('/').get(getIndex)
+    bookRouter.route('/:id').get(getById)
     return bookRouter
 }
 
